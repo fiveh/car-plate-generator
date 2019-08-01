c2b25d5305d8b248f036469f78ad00d67bcd685e
miroshnicehnko.as <miroshnichenko.as@reliab.tech>
2019-08-01 17:20:37 +0300

    +

--- main.py
+++ main.py
@@ -78,4 +78,4 @@ if __name__ == '__main__':
             print(out.shape)
             cv2.imwrite('./numbers_n/created/'+number+region+'.jpg', out)
             print('[SUCCESS WRITE]: {}'.format(number+region))
-            view(out)
+            # view(out)
